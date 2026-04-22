51e2598b378b5be1d43142079c42bbfa4c6ae22a
kkkttt7 <kyawtunnay2000@gmail.com>
2026-04-22 22:03:19 +0700

    Updated

---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const SECRET = "mysecretkey";
+
+// USER
+const user = {
+    id: 1,
+    username: "admin",
+    password: "1234",
+    name: "Kyaw Tun",
+    email: "kyawtun@email.com",
+    bio: "AI Student & Flutter Developer",
+    image: "https://i.pinimg.com/564x/8e/0b/9e/8e0b9e1207dd382176bce6853c46a2b8.jpg"
+};
+
+// MOVIES DATA (with image + description)
+const movies = [
+    {
+        id: 1,
+        title: "Avengers Endgame",
+        year: 2019,
+        image: "https://cdn.marvel.com/content/2x/MLou2_Payoff_1-Sht_Online_DOM_v7_Sm.jpg",
+        description: "Superheroes unite to defeat Thanos and restore balance."
+    },
+    {
+        id: 2,
+        title: "Interstellar",
+        year: 2014,
+        image: "https://m.media-amazon.com/images/I/81kz06oSUeL._AC_UF894,1000_QL80_.jpg",
+        description: "A journey through space to save humanity."
+    },
+    {
+        id: 3,
+        title: "Inception",
+        year: 2010,
+        image: "https://c8.alamy.com/comp/2JH2PW0/movie-poster-inception-2010-2JH2PW0.jpg",
+        description: "A thief steals secrets through dream-sharing technology."
+    }
+];
+
+// LOGIN
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    if (username === user.username && password === user.password) {
+        const token = jwt.sign({ id: user.id }, SECRET);
+
+        res.json({
+            status: "ok",
+            token,
+            name: user.name
+        });
+    } else {
+        res.status(401).json({ message: "Invalid login" });
+    }
+});
+
+// AUTH
+function verifyToken(req, res, next) {
+    const token = req.headers['authorization'];
+    if (!token) return res.sendStatus(403);
+
+    jwt.verify(token, SECRET, (err, decoded) => {
+        if (err) return res.sendStatus(401);
+        req.userId = decoded.id;
+        next();
+    });
+}
+
+// MOVIES
+app.get('/movies', verifyToken, (req, res) => {
+    res.json(movies);
+});
+
+// SINGLE MOVIE
+app.get('/movies/:id', verifyToken, (req, res) => {
+    const movie = movies.find(m => m.id == req.params.id);
+    res.json(movie);
+});
+
+// PROFILE
+app.get('/profile', verifyToken, (req, res) => {
+    res.json(user);
+});
+
+app.listen(3000, () => console.log("Server running"));
